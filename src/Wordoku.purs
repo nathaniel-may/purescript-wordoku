e6745903fa90666aa1c83b44828db6d5f6941842
Nathaniel May <nate.may@fauna.com>
2020-12-30 15:17:12 -0500

    added pruneCells

--- src/Wordoku.purs
+++ src/Wordoku.purs
@@ -1,6 +1,9 @@
 module Wordoku where
 
-import Data.Array (drop, cons, null, take, (..))
+import Prelude
+
+import Data.Array (cons, drop, foldl, null, take, (..))
+import Data.Array as Array
 import Data.Char.Unicode (digitToInt)
 import Data.Maybe (Maybe(..))
 import Data.Set (Set)
@@ -9,7 +12,6 @@ import Data.String (joinWith)
 import Data.String.CodePoints as CodePoints
 import Data.String.CodeUnits (toCharArray)
 import Data.Traversable (traverse)
-import Prelude
 
 exPuzzle :: String
 exPuzzle = ".......1.4.........2...........5.4.7..8...3....1.9....3..4..2...5.1........8.6..."
@@ -47,3 +49,26 @@ readGrid s =
     if CodePoints.length s /= 81
     then Nothing
     else traverse (traverse readCell) (chunksOf 9 $ toCharArray s)
+
+----- solver fns -----
+
+extractOne :: ∀ a. Set a -> Maybe a
+extractOne set = foldl (\_ a -> Just a) Nothing set
+
+-- TODO check usage of set vs array here
+pruneCells :: Array Cell -> Maybe (Array Cell)
+pruneCells cells = traverse pruneCell cells
+  where
+    fixed :: Cell -> Array Int
+    fixed (Fixed x) = [x]
+    fixed _ = []
+
+    diff :: Array Int -> Array Int
+    diff xs = xs `Array.difference` (fixed =<< cells)
+
+    pruneCell :: Cell -> Maybe Cell
+    pruneCell (Possible xs) = case (diff $ Array.fromFoldable xs) of
+        []  -> Nothing
+        [y] -> Just $ Fixed y
+        ys  -> Just (Possible $ Set.fromFoldable ys)
+    pruneCell x = Just x
