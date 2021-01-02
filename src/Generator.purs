8c5c953bfc4a788f8739f044686bc717471f304e
Nathaniel May <nate.may@fauna.com>
2021-01-02 12:03:55 -0500

    added generate

--- src/Generator.purs
+++ src/Generator.purs
@@ -2,12 +2,13 @@ module Generator where
 
 import Prelude
 
-import Data.Array (cons, deleteAt, index, length)
-import Data.Maybe (Maybe, fromMaybe)
-import Data.String.CodeUnits (toCharArray)
+import Data.Array (cons, deleteAt, index, length, replicate, uncons, (..))
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.String.CodeUnits (fromCharArray, toCharArray)
 import Effect (Effect)
 import Effect.Random (randomInt)
-import Solver (CellSet, mkCellSet)
+import Solver (CellSet(..), Cell(..), Grid, mkCellSet, readGrid, replace2D, gridString, solve, solveUnique)
 
 type Opts = { 
       restrictDiag :: Boolean 
@@ -20,8 +21,40 @@ derive instance valueEq :: Eq Values
 data Difficulty = Beginner | Casual | Tricky | Difficult | Challenge | Inhuman
 derive instance difficultyEq :: Eq Difficulty
 
--- generate :: Opts -> Effect String
--- generate = ???
+generate :: Opts -> Effect String
+generate opts = toStringOrLoop =<< do -- may need to generate another puzzle if the difficulty cannot be achieved. Highly unlikely.
+    mCellSet <- cellSet opts.values
+    let mFilled = solve =<< ((\set -> readGrid set emptySudoku) =<< mCellSet)
+    randIdxs <- randomArray (0..80)
+    pure $ do 
+        cs <- mCellSet
+        filled <- mFilled
+        reduceUntil cs (diffNum opts.difficulty) randIdxs filled
+    where
+        reduceUntil :: CellSet -> Int -> Array Int -> Grid -> Maybe Grid
+        reduceUntil cs size idxs grid = do
+            lr <- solveUnique grid
+            { head: idx, tail: rands } <- uncons idxs
+            case lr of 
+                Left _ -> 
+                    if (size <= 0) 
+                    then Just grid
+                    else reduceUntil cs (size - 1) rands (removeAt cs idx grid)
+                -- grid does not have a unique solution, and removing more squares will not improve this.
+                Right _ -> Nothing
+
+        removeAt :: CellSet -> Int -> Grid -> Grid
+        removeAt (CellSet _ allValues) idx grid = 
+            replace2D idx (Possible allValues) grid
+
+        toStringOrLoop :: Maybe Grid -> Effect String
+        toStringOrLoop Nothing = generate opts
+        toStringOrLoop (Just grid) = pure $ gridString grid
+
+cellSet :: Values -> Effect (Maybe CellSet)
+cellSet Numbers = mkCellSet '.' <$> randomArray ['1','2','3','4','5','6','7','8','9']
+cellSet Colors = mkCellSet '.' <$> randomArray ['R','O','Y','L','G','B','I','P','V']
+cellSet (Word word) = mkCellSet '.' <$> randomArray (toCharArray word)
 
 -- fisher yates
 randomArray :: ∀ a. Array a -> Effect (Array a)
@@ -32,11 +65,6 @@ randomArray input = do
         arr <- deleteAt k input
         pure $ cons v <$> (randomArray arr)
 
-cellSet :: Values -> Maybe CellSet
-cellSet Numbers = mkCellSet '.' ['1','2','3','4','5','6','7','8','9']
-cellSet Colors = mkCellSet '.' ['R','O','Y','L','G','B','I','P','V']
-cellSet (Word word) = mkCellSet '.' (toCharArray word)
-
 diffNum :: Difficulty -> Int
 diffNum Beginner  = 62
 diffNum Casual    = 53
@@ -45,5 +73,6 @@ diffNum Difficult = 35
 diffNum Challenge = 26
 diffNum Inhuman   = 17
 
-
+emptySudoku :: String
+emptySudoku = fromCharArray $ replicate 81 '.'
 
