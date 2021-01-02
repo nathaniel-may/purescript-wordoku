a0dceca766265cb94cdf6c3dba740272aed18b2f
Nathaniel May <nate.may@fauna.com>
2021-01-01 20:51:06 -0500

    started interface

---
+++ src/Interface.purs
@@ -0,0 +1,25 @@
+module Interface where
+
+import Prelude
+
+import Data.Maybe (Maybe)
+import Data.String.CodeUnits (toCharArray)
+import Wordoku (CellSet, mkCellSet)
+
+data Values = Numbers | Word String | Colors
+derive instance valueEq :: Eq Values
+
+data Difficulty = Easy | Medium | Hard | VeryHard | Inhumane
+derive instance difficultyEq :: Eq Difficulty
+
+cellSet :: Values -> Maybe CellSet
+cellSet Numbers = mkCellSet '.' ['1','2','3','4','5','6','7','8','9']
+cellSet Colors = mkCellSet '.' ['R','O','Y','L','G','B','I','P','V']
+cellSet (Word word) = mkCellSet '.' (toCharArray word)
+
+type Opts = { 
+      restrictDiag :: Boolean 
+    , values :: Values
+    , difficulty :: Difficulty }
+
+-- generate :: Opts -> String
