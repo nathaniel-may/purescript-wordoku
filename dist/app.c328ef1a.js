693d31cade7cd99f51c67cad2a60089f487271ef
Nathaniel May <nate.may@fauna.com>
2021-01-06 11:12:26 -0500

    deploying

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -50810,7 +50810,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52919" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57487" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
