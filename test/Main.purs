c56a225174adae65813f021cb8d154cb46125c7e
Nathaniel May <nathanieldmay@gmail.com>
2021-01-18 21:36:33 -0500

    add test for solved puzzles

--- test/Main.purs
+++ test/Main.purs
@@ -2,13 +2,17 @@ module Test.Main where
 
 import Prelude
 
-import Data.Array (elem, foldl)
+import Data.Array (all, elem, foldl, group', length, uncons)
+import Data.Array.NonEmpty (toArray)
+import Data.Array.NonEmpty as NonEmptyArray
 import Data.Either (hush)
 import Data.Maybe (fromMaybe)
+import Data.String as String
+import Data.String.CodeUnits (toCharArray)
 import Data.Traversable (sequence, traverse)
 import Effect (Effect)
 import Generator (Difficulty(..), Game(..), generate)
-import Solver (cellSetFromPuzzle, diagonalOf, readGrid, solve)
+import Solver (Grid, cellSetFromPuzzle, diagonalOf, gridString, readGrid, solve)
 import Test.QuickCheck (Result, quickCheck, (<?>))
 import Wordlist (wordlist)
 
@@ -18,14 +22,29 @@ main = do
     void $ traverse quickCheck tests
 
 allTests :: Effect (Array Result)
-allTests = sequence [test1]
+allTests = sequence [test1, test2]
 
+-- solved wordokus have a word from the wordlist on the diagonal
 test1 :: Effect Result
 test1 = do
     str <- generate { difficulty: Beginner, restrictDiag: true, values: Wordoku }
     -- solved relies on all 9 characters being present in the generated puzzle. (not given)
-    let solved = fromMaybe [] $ (solve true) =<< (flip readGrid str) =<< (hush $ cellSetFromPuzzle str)
+    let solved = solveStr true str
     let diag = (foldl (<>) "") <<< (map show) <<< diagonalOf $ solved
     let failureMsg = "Wordoku diagonal failed:\n" <> diag <> "\n" <> str <> "\n"
     pure $ (diag `elem` wordlist) <?> failureMsg
     
+-- solved puzzles have 81 values and 9 of each value.
+test2 :: Effect Result
+test2 = do
+    str <- generate { difficulty: Beginner, restrictDiag: true, values: Sudoku }
+    let solvedStr = gridString $ solveStr false str
+    let total81 = 81 == (String.length solvedStr)
+    let all9 = all (\x -> x == 9) $ map NonEmptyArray.length (group' $ toCharArray solvedStr)
+    let failureMsg = (if total81 then "" else "Solution should have 81 values. It has " <> (show $ String.length solvedStr) <> ". ") <> (if all9 then "" else "Each value doesn't show up 9 times in a solved puzzle.")
+    pure $ (total81 && all9) <?> failureMsg
+
+solveStr :: Boolean -> String -> Grid
+solveStr diag str = fromMaybe [] $ (solve diag)
+    =<< (flip readGrid str)
+    =<< (hush $ cellSetFromPuzzle str)
